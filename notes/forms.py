044a8ea0df3209bdc75147e5c4b3f4d746056c0e
Yunus Kursav <kuy1wz@WZ-C-001TX.wz.emea.bosch.com>
2022-12-28 10:09:18 +0100

    6th chapter validation forms completed

---
+++ notes/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Notes
+from django.core.exceptions import ValidationError
+
+
+class NotesForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ("title", "text")
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control my-5"}),
+            "text": forms.Textarea(attrs={"class": "form-control mb-5"}),
+        }
+        labels = {"text": "Write your thoughts here:"}
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if "DJANGO" not in title:
+            raise ValidationError("We only accept notes about DJANGO!")
+        return title
--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import Http404
 
 from .models import Notes
-from django.views.generic import ListView, DetailView
+from .forms import NotesForm
+from django.views.generic import ListView, DetailView, CreateView
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
 
 
 class NotesListView(ListView):
@@ -15,11 +22,3 @@ class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
     template_name = "notes/notes_detail.html"
-
-
-# def detail(request, pk):
-#     try:
-#         note = Notes.objects.get(pk=pk)
-#     except Notes.DoesNotExist:
-#         raise Http404("Note doesn't exists")
-#     return render(request, "notes/notes_detail.html", {"note": note})

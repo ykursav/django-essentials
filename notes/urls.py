af12c825d7dc69258d209a56b6d3f27926f95126
Yunus Kursav <kuy1wz@WZ-C-001TX.wz.emea.bosch.com>
2022-12-28 08:57:33 +0100

    Chapter 5 - robust frontend completed!

--- notes/urls.py
+++ notes/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("notes", views.list)]
+
+# Can be...
+urlpatterns = [
+    path("notes", views.NotesListView.as_view(), name="notes.list"),
+    path("notes/<int:pk>", views.NotesDetailView.as_view(), name="note.detail"),
+]
--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Notes
+from django.views.generic import ListView, DetailView
 
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, "notes/notes_list.html", {"notes": all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
 
 
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_detail.html"
 
-def detail(request, pk):
-    
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exists")
+#     return render(request, "notes/notes_detail.html", {"note": note})
